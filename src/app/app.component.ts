0b39af32d23245a4c7f4b98bbd434f511d94f7f1
thapaRoyal <thaparoyal17@gmail.com>
2023-02-27 11:21:32 -0500

    input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes-angular';
+  name!: String;
+  onNameChange(value: string) {
+    this.name = value;
+    console.log(this.name);
+  }
 }
